90d2ca32d8247d76c93ed10f16879dc2efeb8971
Tomate77 <aguerofeli21@gmail.com>
2021-11-15 18:00:51 +0100

    pasillos

--- Marmotas-ElJuego/Assets/Scripts/OpenDoor.cs
+++ Marmotas-ElJuego/Assets/Scripts/OpenDoor.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
-    public BoxCollider cameraBox;
-    public Transform transformRotation;
-    public Animator animator;
-
-    void Start() {
-        animator = GetComponent<Animator>();
-    }
-
-    private void OnTriggerEnter(Collider other) 
-    {
-        if(other.CompareTag("Player") && !other.isTrigger)
-        {
-            transformRotation.rotation(y, -90);
-        }
-    }
-
-    private void OnTriggerExit(Collider other) 
-    {
-        if(other.CompareTag("Player") && !other.isTrigger)
-        {
-            transformRotation.rotation(y, 0);
-
-        }
-    }
+    // public BoxCollider cameraBox;
+    // public Transform transformRotation;
+    // public Animator animator;
+    //
+    // void Start() {
+    //     animator = GetComponent<Animator>();
+    // }
+    //
+    // private void OnTriggerEnter(Collider other) 
+    // {
+    //     if(other.CompareTag("Player") && !other.isTrigger)
+    //     {
+    //         transformRotation.rotation(y, -90);
+    //     }
+    // }
+    //
+    // private void OnTriggerExit(Collider other) 
+    // {
+    //     if(other.CompareTag("Player") && !other.isTrigger)
+    //     {
+    //         transformRotation.rotation(y, 0);
+    //
+    //     }
+    // }
 }
